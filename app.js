ee610a32708e8988a7b9ba225f800d1169223598
Heroumi bachir <heroumi25@gmail.com>
2022-10-07 23:11:13 +0100

    responsive des

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const email = document.querySelector('.email'),
 btn = document.querySelector('.footer .primary-btn'),
-errorText = document.querySelector('.error-text');
+errorText = document.querySelector('.error-text'),
+menu = document.querySelector('.menu');
 
 btn.addEventListener('click',()=>{
     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value)) {
         errorText.classList.add('desabled');
     }else{
         errorText.classList.remove('desabled');
+        email.focus()
     }    
+});
+
+
+menu.addEventListener('click',()=>{
+    document.querySelector('.navbar nav').classList.toggle("active");
 });
